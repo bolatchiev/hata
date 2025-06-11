626c7e8821315af4faced95b2c7e69f76cf5cdc3
bolatchiev <bolatchiev.azamat@bk.ru>
2025-06-11 14:30:05 +0300

    RateRouter_DONE

--- server/src/routes/rate.router.js
+++ server/src/routes/rate.router.js
@@ -1,3 +1,11 @@
 const router = require("express").Router();
 const RateController = require("../controllers/Rate.controller");
 
+router
+  .get("/", RateController.getAllRates)
+  .get("/:id", RateController.getRateById);
+//   .post('/', RateController.createRate)
+//   .put('/:id', RateController.updateRate)
+//   .delete('/:id', RateController.deleteRate);
+
+module.exports = router;
