15f145aba510b87720e28d148a6ac6746e2aeee1
bolatchiev <bolatchiev.azamat@bk.ru>
2025-06-11 14:54:15 +0300

    ReviewService_DONE

--- server/src/services/Review.service.js
+++ server/src/services/Review.service.js
@@ -1,4 +1,19 @@
 const { Review, Card, User } = require('../db/models');
 const review = require('../db/models/review');
 
+class ReviewService {
 
+
+  static async addReview({ cardId, userId, text }) {
+    return Review.create({ cardId, userId, text });
+  }
+
+  static async getAllReviewsForCard(cardId) {
+    return Review.findAll({
+      where: { cardId },
+      include: [{ model: User, attributes: ['name'] }],
+      order: [['createdAt', 'DESC']],
+    });
+  }
+}
+module.exports = ReviewService;
