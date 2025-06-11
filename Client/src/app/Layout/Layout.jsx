f89823addd28b75c457077fb708b3dadfd9d8da2
Alyona2809 <topalalyona@yandex.ru>
2025-06-12 00:25:16 +0300

    Mix

--- Client/src/app/Layout/Layout.jsx
+++ Client/src/app/Layout/Layout.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import Header from "../../widgets/Header/Header";
 import Footer from "../../widgets/Footer/Footer";
+import "./layout.css";
+
 export default function Layout({ children, user, setUser }) {
   return (
     <div className="layout">
       <Header user={user} setUser={setUser} />
       <main className="main-content">{children}</main>
-
-      <footer className="footer">
-        <Footer />
-      </footer>
+      <Footer />
     </div>
   );
 }
